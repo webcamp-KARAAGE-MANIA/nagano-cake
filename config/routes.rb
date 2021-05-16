8226c09ceb41521587f6bb4b4e47b6f830132ffd
shuhei05 <qoo0228y@gmail.com>
2021-05-16 08:17:23 +0000

    add genres

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
+devise_for :customers, controllers: {
+  sessions:      'customers/sessions',
+  passwords:     'customers/passwords',
+  registrations: 'customers/registrations'
+}
+
+  namespace :admin do
+    resources :genres
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
