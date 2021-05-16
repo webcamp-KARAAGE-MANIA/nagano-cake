328be80d82f76b34513a8747e248840ee8729dba
yusukenoiri <yusuke.noiri@gmail.com>
2021-05-16 14:44:42 +0000

    add order/order_details controller and view

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,11 +1,15 @@
 class Admin::OrderDetailsController < ApplicationController
 
-  def
-   create
+  def create
+
+    @order_detail = OrderDetail.find(params[:order_detail])
   end
 
-  def
-    update
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    if @order_detail.update(order_detail_params)
+      # 商品のステータスはenumを使用する
+    end
   end
 
 end
