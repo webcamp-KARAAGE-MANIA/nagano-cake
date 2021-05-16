20ed9366d59d0c9fe2c8c6dca8e500fc176f0108
shuhei05 <qoo0228y@gmail.com>
2021-05-16 08:56:33 +0000

    add admin/costomers_controller view

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,16 @@
 class Admin::GenresController < ApplicationController
+
+  def index
+
+  end
+
+  def edit
+
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
