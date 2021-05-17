ea74d647bba97e7fe4bece3101da8a13a2a6d10a
yusukenoiri <yusuke.noiri@gmail.com>
2021-05-17 11:59:39 +0000

    update admin order/order_details view controller

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,15 +1,23 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def create
-
     @order_detail = OrderDetail.find(params[:order_detail])
   end
 
   def update
     @order_detail = OrderDetail.find(params[:id])
     if @order_detail.update(order_detail_params)
-      # 商品のステータスはenumを使用する
+      flash[:success] = "制作ステータスを変更しました!"
+      redirect_to admin_order_path(@order_detail.order)
+    else
+      render "show"
     end
+  end
+
+  private
+  def
+    order_detail_params
+    params.require(:order_detail).permit(:product_status)
   end
 
 end
