142090318a82f92fa5f952f1641a0e47bfb35508
shuhei05 <qoo0228y@gmail.com>
2021-05-17 05:12:48 +0000

    add customer edit show

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,24 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :surname,
+            :name,
+            :surname_kana,
+            :name_kana,
+            :postal_code,
+            :address,
+            :phone_number,
+            :email,
+            :encrypted_password,
+            presence: true
+
+  def full_name
+    self.surename + " " + self.name
+  end
+
+  def full_name_kana
+    self.surename_kana + " " + self.name_kana
+  end
+
 end
