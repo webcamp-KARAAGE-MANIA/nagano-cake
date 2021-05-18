3fee391fc2c1880423d98e93f5bd96f073439e6b
yusukenoiri <yusuke.noiri@gmail.com>
2021-05-18 13:07:22 +0000

    [Add customer show/edit page]

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,17 +1,34 @@
 class CustomersController < ApplicationController
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
     @customer = Customer.find(params[:id])
+
   end
 
   def confirm
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+       redirect_to edit_customer_path
+    else
+       render 'edit'
+    end
   end
 
   def hide
+    @customer = Customer.find(params[:id])
+    @customer.update(is_delete: true)
+    end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :name_kana, :surname, :surname_kana, :postal_code, :address, :phone_number, :email)
   end
+
 end
