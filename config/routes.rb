abe5ec05dc06660ae6aecafccba1d6e07302c802
nakachan1994 <nakachaaaan@gmail.com>
2021-05-19 05:42:02 +0000

    [add]orders/new

--- config/routes.rb
+++ config/routes.rb
@@ -15,13 +15,12 @@ Rails.application.routes.draw do
   get 'homes/about'
   get 'orders/complete', to: 'orders#complete', as: 'complete_order'
   post 'orders/confirm', to: 'orders#confirm', as: 'confirm_order'
- 
+  resources :customers
 
   namespace :admin do
     get 'orders/index'
     get 'orders/show'
   end
-
 
   namespace :admin do
     resources :genres
@@ -30,10 +29,10 @@ Rails.application.routes.draw do
     resources :products, only: [:new, :create, :index, :show, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update,]
   end
-  
+  root :to => 'homes#top'
+  get 'homes/about'
   resources :products, only: [:index, :show]
   resources :cart_items
-
   resources :orders
   resources :shipping_addresses, only: [:new, :create, :index, :edit, :update, :destroy]
   resources :customers
